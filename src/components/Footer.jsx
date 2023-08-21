7a368f37cd4a5c4ffdffcedac9cd533cd4edf6f8
dsk90it <dsk90it@gmail.com>
2023-08-21 14:10:34 +0530

    footer completed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,19 @@
 function Footer() {
-  return <div>Footer</div>
+  return (
+    <div className="flex text-sm justify-between mt-auto bg-white p-4 shadow-[0_0_8px_0_#c2c2c2] -mx-4 lg:-mx-6">
+      <p>{new Date().getFullYear()} &copy; jobsforher</p>
+      <ul className="inline-flex gap-4">
+        <li>
+          <a href="#">About Us</a>
+        </li>
+        <li>
+          <a href="#">Pricing</a>
+        </li>
+        <li>
+          <a href="#">FAQ's</a>
+        </li>
+      </ul>
+    </div>
+  )
 }
 export default Footer
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import LayoutStyles from '../assets/css/layout.module.css'
 import Header from '../components/Header'
 import Sidebar from '../components/Sidebar'
+import Footer from '../components/Footer'
 
 function MainLayout({ children }) {
   const [isSidebarOpen, setSidebarOpen] = useState(true)
@@ -18,6 +19,8 @@ function MainLayout({ children }) {
           }`}
         >
           {children}
+
+          <Footer />
         </div>
       </main>
     </>
